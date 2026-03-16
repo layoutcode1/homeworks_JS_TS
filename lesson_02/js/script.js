d2c40a33e974b2187cd4b973baecd7ea55d802bc
layoutcode1 <7wellspring@gmail.com>
2026-03-16 18:15:03 +0200

    Add files via upload

---
+++ lesson_02/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+// let a: number = 58
+// a = 35 + 25 + 5469
+// console.log(`a=${a}`)
+// document.write(`ok`)
+//___TASK_00___
+const a = 35;
+const b = 29;
+const c = 12;
+const S1 = a + 12 + b;
+console.log(`S1=${S1}`);
+//___TASK_01___
+//___TASK_02___
+//___TASK_03___
+const pricePerUnit = 50.80;
+const quantityOfGoods = 35;
+const totalCoastOfGoods = pricePerUnit * quantityOfGoods;
+const VAT_PERСENTAGE = 5;
+const vatValue = totalCoastOfGoods * VAT_PERСENTAGE / 100;
+console.log(`загальна вартість = ${totalCoastOfGoods}`);
+console.log(`ПДВ від загальної вартості товару = ${vatValue}`);
+//___TASK_04___
+//___TASK_05___
+//___TASK_06___
+//___TASK_07___
---
+++ lesson_02/src/script.ts
@@ -0,0 +1,24 @@
+// let a: number = 58
+// a = 35 + 25 + 5469
+// console.log(`a=${a}`)
+// document.write(`ok`)
+//___TASK-00___
+const a: number = 35
+const b: number = 29
+const c: number = 12
+const S1: number = a + 12 + b
+console.log(`S1=${S1}`)
+//___TASK_01___
+//___TASK_02___
+//___TASK_03___
+const pricePerUnit: number = 50.80
+const quantityOfGoods: number = 35
+const totalCoastOfGoods = pricePerUnit * quantityOfGoods
+const VAT_PERСENTAGE = 5
+const vatValue = totalCoastOfGoods * VAT_PERСENTAGE / 100
+console.log(`загальна вартість = ${totalCoastOfGoods}`)
+console.log(`ПДВ від загальної вартості товару = ${vatValue}`)
+//___TASK_04___
+//___TASK_05___
+//___TASK_06___
+//___TASK_07___
